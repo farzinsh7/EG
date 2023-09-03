bba6007ecf342db2a284dce36e9a99ec68a8ff76
Your Namefarzinsh7 <you@example.com>
2023-09-03 13:56:54 +0330

    add TabularInline to admin panel

--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import IPAddress, User, AccountHits
-from Landing_page.models import MainData, Gallery, MusicPlayer, VideoPlayer, ContactUs
+from Landing_page.models import MainData, Gallery, MusicPlayer, VideoPlayer, ContactUs, SocialLinks
 from django.urls import reverse_lazy
 from .forms import ContactForms, ProfileForms
 from datetime import date
@@ -31,7 +32,7 @@ class Account(LoginRequiredMixin, ListView):
         context['account'] = IPAddress.objects.all().count
         today = datetime.now().date()
         seven_days_ago = today - timedelta(days=7)
-        context['view_data'] = AccountHits.objects.filter(date__range=[seven_days_ago, today])
+        context['view_data'] = list(reversed(AccountHits.objects.filter(date__range=[seven_days_ago, today]).order_by('-date')[:7]))
         return context
 
 
@@ -61,6 +62,48 @@ class Information(LoginRequiredMixin, UpdateView):
     template_name = 'registration/information.html'
     model = MainData
     fields = ['title', 'description', 'header_logo', 'footer_logo']
+    success_url = reverse_lazy('account:info', kwargs={'pk': 1})
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        main_data = self.get_object()
+
+        # Create a formset for SocialLinks
+        SocialLinksFormSet = inlineformset_factory(MainData, SocialLinks, fields=['label', 'link'], extra=0, can_delete=False)
+
+        # Check if there is POST data (form submission) and update the formset accordingly
+        if self.request.method == 'POST':
+            social_links_formset = SocialLinksFormSet(self.request.POST, instance=main_data)
+        else:
+            social_links_formset = SocialLinksFormSet(instance=main_data)
+
+        context['social_links_formset'] = social_links_formset
+
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        social_links_formset = context['social_links_formset']
+
+        if form.is_valid() and social_links_formset.is_valid():
+            # Save the changes to the MainData form
+            main_data = form.save(commit=False)
+            main_data.save()
+
+            # Save the changes to the SocialLinks forms within the formset
+            for social_links_form in social_links_formset:
+                if social_links_form.cleaned_data:
+                    social_links = social_links_form.save(commit=False)
+                    social_links.main_data = main_data  # Set the relationship to the MainData instance
+                    social_links.save()
+
+            return redirect(self.success_url)
+
+    # If the form or formset is not valid, re-render the form with errors
+        return self.render_to_response(self.get_context_data(form=form, social_links_formset=social_links_formset))
+
 
 
 # Gallery admin Panel -------------
--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eg',
         'HOST': 'localhost',
-        'USER': 'eg_user',
+        'USER': 'root',
         'PASSWORD': 'F@rzin123',
     }
 }
