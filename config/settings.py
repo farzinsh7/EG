fb97225507ffa11cec970c71396c60e502cc9d6d
farzinsh7 <farzinnater@gmail.com>
2024-06-29 10:08:48 +0330

    Dynamic social-link

--- config/settings.py
+++ config/settings.py
@@ -92,11 +92,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eg',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'F@rzin123',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
